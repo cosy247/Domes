09f5da79715e78492e851d75c5ae88564ee0294d
cosy247 <705194673@qq.com>
2023-07-13 19:58:15 +0800

    1

--- GeneticAlgorithm-digitsAddUp/index.py
+++ GeneticAlgorithm-digitsAddUp/index.py
@@ -1,4 +1,6 @@
 import random
+from pyecharts.charts import Line
+from pyecharts import options as opts
 
 # 个体数量
 INDIVIDUAL_AMOUNT = 100
@@ -7,22 +9,23 @@ INDIVIDUAL_AMOUNT = 100
 GENE_AMOUNT = 100
 
 # 单个基因突变机率
-GENETIC_LOTTERY_CHANCE = 0.05
+GENETIC_LOTTERY_CHANCE = 0.001
 
 # 交叉变异的机率
-CROSS_VARIATION_CHANCE = 0.02
+CROSS_VARIATION_CHANCE = 0.1
 
 # 迭代次数
-ITERATION_AMOUNT = 30
+ITERATION_AMOUNT = 100
 
-# 获取个体群
+# 获取个体群 
 def getIndividuals() :
 	individuals = []
 	for index in range(INDIVIDUAL_AMOUNT):
 		individual = []
 		for index in range(GENE_AMOUNT):
-			individual.push(random.randint(0, 1))
-		individuals.push(individual)
+			individual.append(random.randint(0, 1))
+		individuals.append(individual)
+	return individuals
 
 
 # 获取个体适应度
@@ -36,13 +39,13 @@ def getNextGenerationIndividuals(individuals):
 	nextExtendIndividuals = []
 	for individual in individuals:
 		fitness = getIndividualFitness(individual)
-		for index in range(fitness // 10):
-			nextExtendIndividuals.push(individual.view())
+		for index in range((fitness * fitness) // 10):
+			nextExtendIndividuals.append(individual[:])
 	# 压缩个数数量
 	extendIndividuals = []
 	nextExtendIndividualsAmount = len(nextExtendIndividuals)
 	for index in range(INDIVIDUAL_AMOUNT):
-		extendIndividuals.push(nextExtendIndividuals[random.randint(0, nextExtendIndividualsAmount)]) #randint和数组长度可以有bug
+		extendIndividuals.append(nextExtendIndividuals[random.randint(0, nextExtendIndividualsAmount - 1)]) #randint和数组长度可以有bug
 	return extendIndividuals
 
 # 对个体进行变异
@@ -53,9 +56,9 @@ def individualGeneLottery(individual):
 
 # 交叉变异
 def individualCrossVariation(individual1, individual2):
-	crossIndex1 = random.randint(GENE_AMOUNT)
-	crossIndex2 = random.randint(GENE_AMOUNT)
-	for index in range(min(crossIndex1, crossIndex2), max(crossIndex1, crossIndex2)):
+	crossIndex1 = random.randint(0, GENE_AMOUNT)
+	# crossIndex2 = random.randint(0, GENE_AMOUNT)
+	for index in range(0, crossIndex1):
 		individual1[index], individual2[index] = individual2[index], individual1[index]
 
 # 对群落进行变异
@@ -63,12 +66,42 @@ def individualsVariation(individuals):
 	for individual in individuals:
 		individualGeneLottery(individual)
 		if(random.random() < CROSS_VARIATION_CHANCE):
-			crossIndividual = individuals[random.randint(INDIVIDUAL_AMOUNT)]
+			crossIndividual = individuals[random.randint(0,INDIVIDUAL_AMOUNT - 1)]
 			individualCrossVariation(individual, crossIndividual)
 
+# 绘制折线图
+def createLineEchart(avgFitnessArr, maxFitnessArr):
+	line=Line()
+	line.set_global_opts(
+        xaxis_opts = opts.AxisOpts(
+            is_show = False,    #隐藏X轴刻度
+        ),
+        yaxis_opts = opts.AxisOpts(
+            is_show = False,    #隐藏Y轴刻度
+        ),
+        legend_opts = opts.LegendOpts(is_show = False),  #隐藏图例
+        title_opts = opts.TitleOpts(title = None),    #隐藏标题
+	)
+	line.add_xaxis(range(1, ITERATION_AMOUNT))
+	line.add_yaxis('平均适应度', avgFitnessArr, label_opts = opts.LabelOpts(is_show=False))
+	line.add_yaxis('最高适应度',maxFitnessArr)
+	line.set_global_opts(title_opts = opts.TitleOpts(title="OneMax"))
+	line.render('./index.html')
+
+# 程序入口
 if __name__ =='__main__':
 	individuals = getIndividuals()
+	avgFitnessArr = []
+	maxFitnessArr = []
 	for index in range(ITERATION_AMOUNT):
 		individuals = getNextGenerationIndividuals(individuals)
 		individualsVariation(individuals)
-	print(individuals)
+		allFitness = 0
+		maxFitness = 0
+		for individual in individuals:
+			fitness = getIndividualFitness(individual)
+			allFitness += fitness
+			maxFitness = maxFitness if maxFitness > fitness else fitness
+		avgFitnessArr.append(allFitness / INDIVIDUAL_AMOUNT)
+		maxFitnessArr.append(maxFitness)
+	createLineEchart(avgFitnessArr, maxFitnessArr)
